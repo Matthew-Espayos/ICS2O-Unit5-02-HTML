5b351d4d095c94e27ba541a787a8c0da42c34a6b
MatthewEspayos <>
2021-06-01 16:12:52 +0000

    Javascript and CSS mistakes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// The code below will make the "Hello, Matthew Espayos!" turn to #2F2F2F and it will be larger than it's original size. 
-document.getElementById("p1").style.color = "#2F2F2F";
-document.getElementById("p1").style.fontSize = "larger";
-// The code below will display the background color to #D6E3F8. 
-document.getElementById("background").style.backgroundColor = "#D6E3F8";
-// The code below will give the button properties
-document.getElementById("myButton").addEventListener("click", myFunction);
-function myFunction() {
-  alert ("Hello, world!");
+// The code below will make the "Hello, Matthew Espayos!" turn to #2f2f2f and it will be larger than it's original size.
+document.getElementById("p1").style.color="#2f2f2f"
+document.getElementById("p1").style.fontSize="larger"
+// The code below will display the background color to #d6e3f8.
+document.getElementById("background").style.backgroundColor="#d6e3f8"
+// The code below will give the button properties.
+document.getElementById("myButton").addEventListener("click", myFunction)
+function myFunction () {
+  alert("Hello, world!")
 }
